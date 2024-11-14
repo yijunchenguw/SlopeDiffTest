6a81b48648f8bd5d4adde62d023b0d6f76e9a1c4
yijunchenguw <chengxb@uw.edu>
2024-11-13 22:37:50 -0800

    Test Example

---
+++ Example_SlopeDiffTest.R
@@ -0,0 +1,16 @@
+## Preliminaries ----
+### Call libraries ----
+library(lme4) # model
+
+### Load raw dataset ----
+data("mlbook_red_b_subset")
+
+## Model ----
+Ex1_model <- lmer(aritPOST ~ 1 + ZlangPOST_CMC + ZIQ_verb_CMC + Zses_CMC
+                  + ZlangPOST_agg + ZIQ_verb_agg + Zses_agg
+                  + (1|schoolnr),
+                  data = mlbook_red_b_subset, REML = FALSE, control = lmerControl(optimizer="nloptwrap"))
+
+## Run Slopediff_all_pairs test
+slopediff_all_pairs(Ex1_model)
+
